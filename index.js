5e007365098be00cbfe8e56f4e4c071f06513ad0
Valencia White <valenciawhite@rocketmail.com>
2022-02-25 00:51:01 -0500

    commit tree 260198adc5fdebef91cd82c2a1f80d3fd7195eaa

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Code your solution in this file!
+let headQuartLocation = 42;
+
+function distanceFromHqInBlocks(pickupLocation){
+    return Math.abs(pickupLocation - headQuartLocation)
+}
+
+let blockToFeet = 264;
+
+function distanceFromHqInFeet(pickupLocation){
+   let distance = distanceFromHqInBlocks(pickupLocation);
+   return distance * blockToFeet
+}
+
+function distanceTravelledInFeet(start, destination){
+    return Math.abs((start - destination) * blockToFeet)
+}
+
+
+function calculatesFarePrice(start, destination){
+    let feet = distanceFromHqInFeet(start, destination);
+
+    if(feet <= 400){
+        return 0;
+    } else if (feet > 400 && feet < 2000){
+        return (feet - 400) * 0.02;
+    } else if(feet > 2000) {
+        return 25;
+    }else{
+        return 'cannot travel that far';
+    }
+
+}
+
+
